b1bdf5b72167dcc0867197adf9d2303375ac23b5
sloscos <sloscos@uoc.edu>
2025-05-13 00:58:11 +0200

    Camara AR funcionando
    Funciona juego ar y juego 2d_3. faltaría arreglar sonido fondo pero es mejorar esperar a unir todas las escenas

---
+++ Assets/Scripts/ToqueFantasma.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ToqueFantasma : MonoBehaviour
+{
+    public int puntosPorFantasma = 50;
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("Fantasma"))
+                {
+                    Destroy(hit.collider.gameObject);
+
+                    ARGhostSpawner spawner = FindFirstObjectByType<ARGhostSpawner>();
+                    if (spawner != null)
+                    {
+                        spawner.SumarPuntos(puntosPorFantasma);
+                    }
+                }
+            }
+        }
+    }
+}
