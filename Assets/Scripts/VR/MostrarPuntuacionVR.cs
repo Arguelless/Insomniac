c4241a6ba9db686c3e5f8b28864626fec40ff21a
MiguelGarzo <mgarzom@uoc.edu>
2025-05-15 15:33:07 +0200

    Juego VR con puntuación sumada añadido al bucle.

--- Assets/Scripts/VR/MostrarPuntuacionVR.cs
+++ Assets/Scripts/VR/MostrarPuntuacionVR.cs
@@ -10,5 +10,14 @@ public class MostrarPuntuacionFin : MonoBehaviour
         int puntuacion = PlayerPrefs.GetInt("PuntuacionFinVR", -1);
         Debug.Log("Puntuación guardada: " + puntuacion);
         textoFinal.text = "Tu puntuación en la DjExperience: " + puntuacion;
+
+        if (PuntuacionManager.Instance != null)
+        {
+            PuntuacionManager.Instance.AsignarPuntos(4, puntuacion);
+        }
+        else
+        {
+            Debug.LogError("La instancia de PuntuacionManager no está disponible.");
+        }
     }
 }
